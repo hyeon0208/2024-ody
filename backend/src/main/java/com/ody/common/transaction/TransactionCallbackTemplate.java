7f382e0dbe9e7e5948953c4309a38da3fdb6d3af
H <dmsgktn0208@gmail.com>
2025-03-20 15:39:42 +0900

    feat: 트랜잭션 콜백 작업을 담당하는 클래스 추가

---
+++ backend/src/main/java/com/ody/common/transaction/TransactionCallbackTemplate.java
@@ -0,0 +1,30 @@
+package com.ody.common.transaction;
+
+import java.util.function.Supplier;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TransactionCallbackTemplate {
+
+    private final TransactionTemplate transactionTemplate;
+
+    public <T> T executeWithAfterCommitAction(Supplier<T> action, Runnable afterCommitAction) {
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+            @Override
+            public void afterCompletion(int status) {
+                afterCommitAction.run();
+                log.debug("트랜잭션 커밋 후 추가 작업 실행 완료");
+            }
+        });
+
+        // 트랜잭션 내에서 작업 실행
+        return transactionTemplate.execute(transactionStatus -> action.get());
+    }
+}
